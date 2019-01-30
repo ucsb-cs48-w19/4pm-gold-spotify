f6ee7b0260735154f4887dbef44488c3ab5b318f
aprilsanchez <sapril338@gmail.com>
2019-01-29 16:23:34 -0800

    Finished hello_april.py

--- hello_april.py
+++ hello_april.py
@@ -13,7 +13,7 @@ white = (255,255,255)
 red = (255,0,0)
 green = (0,200,0)
 bright_green = (0,255,0)
-bright_red = (250,0,0)
+bright_red = (255,69,0)
 
 displayInfo = pygame.display.Info()
 displayHeight = displayInfo.current_h//3
