6cfc496206ff7749035267a60cdea498f1008c87
tinieray <43763562+tinieray@users.noreply.github.com>
2019-01-29 23:35:07 -0800

    Update hello_ray.py

--- ray/hello_ray.py
+++ ray/hello_ray.py
@@ -18,31 +18,21 @@ blue = (0, 0, 255)
 display_width = 800
 display_height = 600
 
+smallText = pygame.font.SysFont('comicsansms', 20)
+largeText = pygame.font.SysFont('comicsansms', 115)
+
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
 
-#Img = pygame.image.load('cloud.jpeg')
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
-#def staticObject(x, y):
-#    gameDisplay.blit(Img, (x, y))
-
-def message_diaplay(text):
-    largeText = pygame.font.SysFont('comicsansms',115)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2), (display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
-    pygame.display.update()
-    time.sleep(1)
-    game_loop()
 
 def button(msg, x, y, w, h, ic, ac, action = None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
-    
     print(click)
     if x + w > mouse[0] > x and y + h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
@@ -51,14 +41,15 @@ def button(msg, x, y, w, h, ic, ac, action = None):
 
         else:
             pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
-            smallText = pygame.font.SysFont('comicsansms', 20)
             textSurf, textRect = text_objects(msg, smallText)
             textRect.center = ((x + (w / 2)), (y + (h / 2)))
             gameDisplay.blit(textSurf, textRect)
 
+
 def quitgame():
     pygame.quit()
     quit()
+
 
 def game_intro():
     intro = True
@@ -68,15 +59,15 @@ def game_intro():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        gameDisplay.fill(grey)
-        largeText = pygame.font.SysFont('comicsansms',115)
-        TextSurf, TextRect = text_objects('Hello World!', largeText)
+
+        gameDisplay.fill(white)
+        TextSurf, TextRect = text_objects('Turkey-Trot!', largeText)
         TextRect.center = ((display_width/2), (display_height/2))
         gameDisplay.blit(TextSurf, TextRect)
         button("Start", 150, 450, 100, 50, green, bright_green, game_loop)
         button("Quit", 550, 450, 100, 50, red, bright_red, quitgame)
         pygame.display.update()
-        clock.tick(5)
+        clock.tick(60)
 
 
 def game_loop():
@@ -89,7 +80,6 @@ def game_loop():
                 quit()
 
         gameDisplay.fill(white)
-        largeText = pygame.font.SysFont('comicsansms', 115)
         TextSurf, TextRect = text_objects('Blank Page', largeText)
         TextRect.center = ((display_width / 2), (display_height / 2))
         gameDisplay.blit(TextSurf, TextRect)
@@ -97,14 +87,14 @@ def game_loop():
         button("Again?", 150, 450, 100, 50, blue, bright_green, game_intro)
         button("Quit?", 550, 450, 100, 50, red, bright_red, quitgame)
         pygame.display.update()
-        clock.tick(5)
+        clock.tick(60)
+
 
 pygame.display.update()
 clock.tick(60)
 
 
 game_intro()
-game_loop()
 pygame.quit()
 quit()
 
