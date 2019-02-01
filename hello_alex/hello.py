c1ae24cae963eb3f66e84cf940c84962006cf557
shenmo3 <ahliu@umail.ucsb.edu>
2019-01-31 16:27:50 -0800

    Base Game

--- hello_alex/hello.py
+++ hello_alex/hello.py
@@ -9,6 +9,9 @@ BRIGHT_GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WIDTH = 800
 HEIGHT = 600
+pygame.font.init()
+BASICFONT = pygame.font.SysFont(None, 50)
+SMALLFONT = pygame.font.SysFont(None, 25)
 
 
 class Display:
@@ -17,80 +20,126 @@ class Display:
         self.clock = pygame.time.Clock()
         self.clock.tick(30)
         self.canvas = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-        self.basicFont = pygame.font.SysFont(None, 50)
-        self.mouse = pygame.mouse.get_pos()
-        self.click = pygame.mouse.get_pressed()
-        self.start_time = 0
-        # start = 0, in game = 1, end game = 2
-        self.state = 0
+        self.events = []
+        self.world = World()
 
     def start(self):
         self.refresh()
+
+    def draw_game(self, texts, buttons, objects):
+        self.canvas.fill(WHITE)
+        for button in buttons:
+            pygame.draw.rect(self.canvas, button[4], (button[0], button[1], button[2], button[3]))
+        for obj in objects:
+            pygame.draw.ellipse(self.canvas, BLACK, (obj.x, obj.y, 25, 50))
+        for t in texts:
+            text = t[4].render(t[0], True, t[1])
+            textRect = text.get_rect()
+            textRect.center = (t[2], t[3])
+            self.canvas.blit(text, textRect)
+        pygame.display.update()
+
+    def refresh(self):
+        while True:
+            self.events = pygame.event.get()
+            for event in self.events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+            texts, buttons, objects = self.world.refresh(self.events)
+            self.draw_game(texts, buttons, objects)
+
+
+class World:
+    def __init__(self):
+        self.player = Player()
+        self.events = []
+        self.buttons = []
+        self.texts = []
+        self.objects = []
+        self.mouse = pygame.mouse.get_pos()
+        self.click = pygame.mouse.get_pressed()
+        # start = 0, in game = 1, end game = 2
+        self.state = 0
+        self.start_time = 0
 
     def update(self):
         self.mouse = pygame.mouse.get_pos()
         self.click = pygame.mouse.get_pressed()
 
-    def draw_game(self):
+    def run(self):
+        self.buttons = []
+        self.texts = []
+        self.objects = []
         if self.state == 0:
-            pygame.display.set_caption('Hello world')
-            text = self.basicFont.render('Hello world!', True, WHITE, BLUE)
-            textRect = text.get_rect()
-            textRect.center = (self.canvas.get_rect().centerx, 100)
-            self.canvas.fill(WHITE)
-            self.canvas.blit(text, textRect)
+            self.texts.append(('Hello world!', BLACK, 400, 100, BASICFONT))
+            self.texts.append(('Start Game', BLACK, 400, 375, SMALLFONT))
             if 350 + 100 > self.mouse[0] > 350 and 350 + 50 > self.mouse[1] > 350:
-                pygame.draw.rect(self.canvas, BRIGHT_GREEN, (350, 350, 100, 50))
+                self.buttons.append((350, 350, 100, 50, BRIGHT_GREEN))
                 if self.click[0] == 1:
                     self.state = 1
                     self.start_time = time.time()
             else:
-                pygame.draw.rect(self.canvas, GREEN, (350, 350, 100, 50))
-            smallText = pygame.font.SysFont(None, 25)
-            start_text = smallText.render('Start Game', True, WHITE)
-            start_text_Rect = start_text.get_rect().center = (400, 375)
-            start_text_Rect.center = (400, 375)
-            self.canvas.blit(start_text, start_text_Rect)
+                self.buttons.append((350, 350, 100, 50, GREEN))
 
         elif self.state == 1:
-            self.canvas.fill(WHITE)
-            text = self.basicFont.render('In Game', True, WHITE, BLUE)
-            textRect = text.get_rect()
-            textRect.center = (self.canvas.get_rect().centerx, 100)
-            if time.time() - self.start_time >= 5:
+            # TODO: LINE
+            # pygame.draw.line(self.canvas, BLACK, (0, 500), (800, 500))
+            self.objects.append(self.player)
+            if time.time() - self.start_time >= 10:
                 self.state = 2
 
         elif self.state == 2:
-            text = self.basicFont.render('Game Over!', True, BLACK)
-            textRect = text.get_rect()
-            textRect.center = (self.canvas.get_rect().centerx, 100)
-            self.canvas.fill(WHITE)
-            self.canvas.blit(text, textRect)
+            self.texts.append(('Game Over!', BLACK, 400, 100, BASICFONT))
+            self.texts.append(('Start Again', WHITE, 400, 375, SMALLFONT))
             if 350 + 100 > self.mouse[0] > 350 and 350 + 50 > self.mouse[1] > 350:
-                pygame.draw.rect(self.canvas, BRIGHT_GREEN, (350, 350, 100, 50))
+                self.buttons.append((350, 350, 100, 50, BRIGHT_GREEN))
                 if self.click[0] == 1:
                     self.state = 1
                     self.start_time = time.time()
+                    self.player = Player()
             else:
-                pygame.draw.rect(self.canvas, GREEN, (350, 350, 100, 50))
-            smallText = pygame.font.SysFont(None, 25)
-            start_text = smallText.render('Start Again', True, WHITE)
-            start_text_Rect = start_text.get_rect()
-            start_text_Rect.center = (400, 375)
-            self.canvas.blit(start_text, start_text_Rect)
-
+                self.buttons.append((350, 350, 100, 50, GREEN))
         else:
             print("Unknown state", self.state)
-        pygame.display.update()
 
-    def refresh(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-            self.update()
-            self.draw_game()
+    def refresh(self, events):
+        self.update()
+        self.events = events
+        self.run()
+        self.player.refresh(self.events)
+        return self.texts, self.buttons, self.objects
+
+
+class Player:
+    def __init__(self, x=50, y=450):
+        self.x = x
+        self.y = y
+        self.events = []
+        self.pressed = None
+
+    def update(self, events):
+        self.events = events
+
+    def refresh(self, events):
+        self.update(events)
+        if self.pressed is not None:
+            if self.pressed == pygame.K_a and self.x >= 10:
+                self.x -= 10
+            elif self.pressed == pygame.K_d and self.x < 780:
+                self.x += 10
+        for event in self.events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a and self.x >= 10:
+                    self.x -= 10
+                    self.pressed = pygame.K_a
+                elif event.key == pygame.K_d and self.x < 780:
+                    self.x += 10
+                    self.pressed = pygame.K_d
+                else:
+                    print("Unknown key")
+            elif event.type == pygame.KEYUP and self.pressed is not None and event.key == self.pressed:
+                self.pressed = None
 
 
 if __name__ == "__main__":
