27e731a085059c1b4644f65c27d387e5df1971fb
JacquelineMai <jacquelinemai@umail.ucsb.edu>
2019-02-20 17:29:20 -0800

    Animation working for player sprite- replace frames with our own art

--- hello_alex/player.py
+++ hello_alex/player.py
@@ -1,5 +1,5 @@
 import time, pygame
-
+from gameConstants import Color
 
 class Player:
     def __init__(self, x=50, y=450):
@@ -11,14 +11,41 @@ class Player:
         self.health = 100
         self.score = 0
         self.immune_time = 0
-        self.image = pygame.Surface([x, y], pygame.SRCALPHA)
-        self.image = pygame.image.load("player.png").convert_alpha()
+	# self.image = pygame.Surface([x, y], pygame.SRCALPHA)
+	# self.image = pygame.image.load("player.png").convert_alpha()
+        self.images = []
+        self.images.append(pygame.image.load('minWalk1.png').convert_alpha())
+        self.images.append(pygame.image.load('minWalk2.png').convert_alpha())
+        self.images.append(pygame.image.load('minWalk3.png').convert_alpha())
+        self.images.append(pygame.image.load('minWalk4.png').convert_alpha())
+        self.images.append(pygame.image.load('minWalk5.png').convert_alpha())
+        self.images.append(pygame.image.load('minWalk6.png').convert_alpha())
+        self.index = 0
+        self.state = 0
+        self.image = self.images[self.index]
+#        self.transparent = self.image.get_at((0,0))
+#        print(self.transparent)
+#       print(Color.BLACK.value)
+#       self.image.set_colorkey(self.transparent)
+
+#        self.rect = self.image[self.index].get_rect()
+        # this line from original code
         self.image = pygame.transform.scale(self.image, (50, 75))
 
     def update(self, events):
         self.events = events
 
     def move(self, direction):
+        if self.state == 0:
+            self.state+=1 
+            self.index+=1
+        else:
+            self.state = 0
+        if self.index>=len(self.images):
+            self.index=0
+        self.image = self.images[self.index]
+        self.image = pygame.transform.scale(self.image, (50, 75))
+
         if direction == 'left' and self.x >= 10:
             self.x -= 10
         elif direction == 'right' and self.x < 780:
