572001519c366ec0215f34bf40c3d7f95d79c104
aprilsanchez <sapril338@gmail.com>
2019-03-04 23:05:44 -0800

    fixed speed

--- source/player.py
+++ source/player.py
@@ -18,7 +18,7 @@ class Player:
         self.isJump = 0
         self.v = 10
         self.m = 2
-        self.speed = 3
+        self.speed = 8
 
     def update(self, events):
         self.events = events
